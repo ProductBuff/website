9e7ad92b6da348c204d0d6a8e79b39e858e37a89
Gabriel Carpenedo <cbgabe@gmail.com>
2016-09-01 10:11:19 -0400

    Changes sales page route name

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#homepage'
-  get 'services', to: 'pages#services', as: :services
+  get 'rpp', to: 'pages#services', as: :services
   resources :contacts, only: :create
 end
